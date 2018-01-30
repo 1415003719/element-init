6b79bca19166387bb4596694355da0ed2fec031e
watson <1415003719@qq.com>
2018-01-31 00:17:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="4">
+      <h5>默认颜色</h5>
+      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+    <el-col :span="20">
+      <h1>Content</h1>
+    </el-col>
+  </el-row>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
